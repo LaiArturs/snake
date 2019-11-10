9a5b3c3fc21b82129b4e6862078532dd27210f4d
Arturs Laizans <arturs.laizans@hotmail.com>
2019-11-10 21:38:46 +0200

    Implement gameSnake as linked list

--- snake.c
+++ snake.c
@@ -3,18 +3,36 @@
 #include <stdlib.h>  // for srand()
 #include <time.h>  // for time()
 
+#define START_LENGTH 5
+
 char getDirection(char cd, WINDOW *win);
+
+typedef struct snakeSection {
+    short int y;
+    short int x;
+    struct snakeSection *next;
+} snakeSection_t;
+
+
+typedef struct {
+    unsigned int length;
+    snakeSection_t *head;
+    char direction;  // 'U' - UP, 'D' - down, 'L' - left, 'R' - right
+} snake_t;
+
+snake_t initializeSnake(void);
+void removeSnakeEndFromDisplay(snake_t snake, WINDOW *win);
+void drawSnake(snake_t snake, WINDOW *win);
+void advanceSnakeForward(snake_t *snake);
+snakeSection_t *returnTail(snake_t snake);
 
 int main(void)
 {   
-    int maxSnakeLength = 100;
-    int snake[maxSnakeLength][2];
-    int snakeLength = 5;
-    char dirrection = 'R'; // 'U' - UP, 'D' - down, 'L' - left, 'R' - right
     char newDirrection;
     char foodDisplayed = 0;  // 0 - not displayed, 1 - is displayed
     unsigned int food[2];
     unsigned int score = 0;
+    snakeSection_t *currentSection;  // to iterate over snake
 
     initscr();  // initialize curses
     WINDOW *win = newwin(LINES-2, COLS-2, 1, 1);
@@ -23,11 +41,7 @@ int main(void)
     nodelay(win, TRUE);  // use non blocking for getch()
     keypad(win, TRUE);  // allow special keys
     curs_set(0);  // hide cursor
-    for (size_t i = 0; i < snakeLength; i++)
-    {
-        snake[i][0] = LINES/2;
-        snake[i][1] = (COLS/2)-i;
-    }
+    snake_t gameSnake = initializeSnake();
     
     //box(win, ACS_VLINE, ACS_HLINE );
     box(win, '|', ACS_HLINE );
@@ -35,61 +49,34 @@ int main(void)
     //mvwprintw( win, 10, 15, "%c", 64 );wrefresh(win);
 
     // draw first snake
-    mvwaddch( win, snake[0][0], snake[0][1], '&');
-    for (size_t i = 1; i < snakeLength; i++) {   
-        mvwaddch( win, snake[i][0], snake[i][1], '@');
-    }
+    drawSnake(gameSnake, win);
+
     wrefresh(win);
     while (1)
     {
         usleep(100000);
-
         // check if food eaten
         if (foodDisplayed) {
-            if (snake[0][0] == food[0] && snake[0][1] == food[1]){
+            if (gameSnake.head->y == food[0] && gameSnake.head->x == food[1]) {
                 foodDisplayed = 0;
-                snakeLength++;
+                gameSnake.length++;
                 score++;
             }
         }
-
-
         // remove last bit of snake
-        mvwaddch(win, snake[snakeLength - 1][0], snake[snakeLength - 1][1], ' ');
-        // Get dirrection from user
-        newDirrection = getDirection(dirrection, win);
-        if (newDirrection != 0) {
-            dirrection = newDirrection;
-        }
-        // recalculate snake
-        for (size_t i = snakeLength; i > 0; i--) {
-            snake[i][0] = snake[i-1][0];
-            snake[i][1] = snake[i-1][1];
-        }
-        switch (dirrection)
-        {
-        case 'U':
-            snake[0][0]--;
-            break;
-        case 'D':
-            snake[0][0]++;
-            break;
-        case 'R':
-            snake[0][1]++;
-            break;
-        case 'L':
-            snake[0][1]--;
-            break;
+        removeSnakeEndFromDisplay(gameSnake, win);
         
-        default:
-            // Error case
-            break;
+        // Get dirrection from user
+        newDirrection = getDirection(gameSnake.direction, win);
+        if (newDirrection != 0) {
+            gameSnake.direction = newDirrection;
         }
-
+        
         // move snake head forward
-        mvwaddch(win, snake[0][0], snake[0][1], '&'); //head
-        mvwaddch(win, snake[1][0], snake[1][1], '@'); //first part of neck
-
+        advanceSnakeForward(&gameSnake);
+        mvwaddch(win, gameSnake.head->y, gameSnake.head->x, '&'); //head
+        mvwaddch(win, gameSnake.head->next->y, gameSnake.head->next->x, '@'); //first part of neck
+        snakeSection_t *tail = returnTail(gameSnake);
         // calculate food location
         if (!foodDisplayed) {
             unsigned int r;
@@ -100,12 +87,13 @@ int main(void)
                 food[0] = (r % (LINES - 4)) + 1;
                 food[1] = (r % (COLS - 4)) + 1;
                 foodOnSnake = 0;
-                for (size_t i = 0; i < snakeLength; i++)
-                {
-                    if (snake[i][0] == food[0] && snake[i][1] == food[1]) {
+                currentSection = gameSnake.head;
+                while (currentSection!=NULL) {
+                    if (currentSection->y == food[0] && currentSection->x == food[1]) {
                         foodOnSnake = 1;
                         break;
                     }
+                    currentSection = currentSection->next;
                 }    
             } while (foodOnSnake);
             mvwaddch(win, food[0], food[1], '*');
@@ -115,9 +103,106 @@ int main(void)
 
         wrefresh(win);
     }
-    
     return 0;
 }
+
+snake_t initializeSnake(void) {
+    snake_t gameSnake;
+    gameSnake.length = 0;
+    gameSnake.direction = 'R';
+    snakeSection_t *currentSection = NULL;
+    currentSection = malloc(sizeof(snakeSection_t));
+    if (currentSection==NULL) {
+        // could not allocate memory for snake section
+        return gameSnake;
+    }
+    gameSnake.head = currentSection;
+    
+    for (int i = 0; i < 5; i++) {
+        currentSection->y = LINES/2;
+        currentSection->x = (COLS/2)-i;
+        currentSection->next = NULL;
+        gameSnake.length++;
+        if (i != 5-1){
+            currentSection->next = malloc(sizeof(snakeSection_t));
+            if (currentSection==NULL) {
+                // could not allocate memory for snake section
+                return gameSnake;
+            }
+            currentSection = currentSection->next;    
+        }
+    }
+    
+    return gameSnake;
+}
+
+void removeSnakeEndFromDisplay(snake_t snake, WINDOW *win) {
+    snakeSection_t *currentSection = snake.head;
+    while (currentSection->next!=NULL) {
+        currentSection = currentSection->next;
+    }
+    mvwaddch(win, currentSection->y, currentSection->x, ' ');
+}
+
+void drawSnake(snake_t snake, WINDOW *win) {
+    snakeSection_t *currentSection = snake.head;
+    mvwaddch(win, currentSection->y, currentSection->x, '&');
+    while (currentSection->next!=NULL)
+    {
+        currentSection = currentSection->next;
+        mvwaddch(win, currentSection->y, currentSection->x, '@');
+    }
+}
+
+void advanceSnakeForward(snake_t *snake) {
+    snakeSection_t *newHead = NULL;
+    newHead = malloc(sizeof(snakeSection_t));
+    newHead->x = snake->head->x;
+    newHead->y = snake->head->y;
+    newHead->next = snake->head;
+    snake->head = newHead;
+    
+    switch (snake->direction)
+        {
+        case 'U':
+            newHead->y--;
+            break;
+        case 'D':
+            newHead->y++;
+            break;
+        case 'R':
+            newHead->x++;
+            break;
+        case 'L':
+            newHead->x--;
+            break;
+        
+        default:
+            // Error case
+            break;
+        }
+
+    snakeSection_t *currentSection = snake->head;
+    for (size_t i = 1; i < snake->length; i++)
+    {
+        currentSection = currentSection->next;
+    }
+
+    if (currentSection->next != NULL) {
+        free(currentSection->next);
+        currentSection->next=NULL;
+    } 
+}
+
+snakeSection_t *returnTail(snake_t snake) {
+    snakeSection_t *currentSection = snake.head;
+    while (currentSection->next!=NULL)
+    {
+        currentSection = currentSection->next;
+    }
+    return currentSection;    
+}
+
 
 char getDirection(char cd, WINDOW *win) {
     int ch;
